88d64ad3ce7c600d94aa084a0c453e6cf744bd8f
Mike-Benn <mbenn49@wgu.edu>
2023-12-30 09:12:12 -0500

    Add variables for all btns and screen

--- scripts.js
+++ scripts.js
@@ -2,6 +2,24 @@ let operator = null;
 let firstNum = null;
 let secondNum = null;
 
+const screenText = document.querySelector('.screen-text');
+const deleteBtn = document.querySelector('#delete-btn');
+const clearBtn = document.querySelector('#clear-btn');
+const oneBtn = document.querySelector('#one-btn');
+const twoBtn = document.querySelector('#two-btn');
+const threeBtn = document.querySelector('#three-btn');
+const fourBtn = document.querySelector('#four-btn');
+const fiveBtn = document.querySelector('#five-btn');
+const sixBtn = document.querySelector('#six-btn');
+const sevenBtn = document.querySelector('#seven-btn');
+const eightBtn = document.querySelector('#eight-btn');
+const nineBtn = document.querySelector('#nine-btn');
+const decimalBtn = document.querySelector('#decimal-btn');
+const timesBtn = document.querySelector('#times-btn');
+const divideBtn = document.querySelector('#divide-btn');
+const addBtn = document.querySelector('#add-btn');
+const minusBtn = document.querySelector('#minus-btn');
+
 
 
 function operate(operator , num1 , num2) {
@@ -15,3 +33,9 @@ function operate(operator , num1 , num2) {
         return num1 - num2;
     }
 }
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+  });
