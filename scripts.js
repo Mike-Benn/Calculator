34fb620c9332fe0f1a25f92ab6003c8555e545a6
Mike-Benn <mbenn49@wgu.edu>
2023-12-30 21:20:14 -0500

    Add delete,clear,seven button functions

--- scripts.js
+++ scripts.js
@@ -21,6 +21,34 @@ const addBtn = document.querySelector('#add-btn');
 const minusBtn = document.querySelector('#minus-btn');
 
 
+function del() {
+    deleteBtn.addEventListener('click' , () => {
+        if (screenText.textContent.length == 1) {
+            screenText.textContent = "0";
+        } else {
+            screenText.textContent = screenText.textContent.slice(0 , screenText.textContent.length - 1);
+        }
+    })
+
+}
+
+function clear() {
+    clearBtn.addEventListener('click' , () => {
+        screenText.textContent = "0";
+    })
+}
+
+function numberSeven() {
+    sevenBtn.addEventListener('click' , () => {
+        if (screenText.textContent.length < 14) {
+            if (screenText.textContent === "0") {
+                screenText.textContent = "7";
+            } else {
+                screenText.textContent = screenText.textContent + "7"
+            }
+        }
+    })
+}
 
 function operate(operator , num1 , num2) {
     if (operator === "/") {
@@ -37,5 +65,9 @@ function operate(operator , num1 , num2) {
 
 
 document.addEventListener("DOMContentLoaded", () => {
+    clear();
+    del();
+    numberSeven();
+
     
   });
